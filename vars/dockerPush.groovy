f729556533758d15e1fcf46e2d845b7696b4ba8d
TaskMasterErnest <ernestklu08@gmail.com>
2023-03-17 08:38:12 +0000

    modded a groovy script

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -2,7 +2,7 @@
 
 def call(String imageName) {
     echo "logging into Docker ..."
-    withCredentials([usernamePassword(credentialsId: "docker-hub-repo", usernameVariable: 'USER', passwordVariable: 'PASS')]){
+    withCredentials([usernamePassword(credentialsId: "docker-hub-repo", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]){
         echo $PASS | docker login -u $USER --password-stdin
         sh "docker push $imageName"
     }
